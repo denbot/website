9548cf363b1648842e4aa3a792cf46e486626dfe
Monique Streetman <moniquestreetman@gmail.com>
2025-09-21 14:03:17 -0600

    Update backend to generate jwts

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -4,6 +4,10 @@ from rest_framework.response import Response
 import os
 from twilio.rest import Client
 
+import jwt
+from datetime import datetime, timedelta, timezone
+from django.conf import settings
+
 api_key = os.environ["TWILIO_API_KEY"]
 api_secret = os.environ["TWILIO_API_SECRET"]
 account_sid = os.environ["TWILIO_ACCOUNT_SID"]
@@ -17,20 +21,44 @@ class LoginAPIView(APIView):
         phone_number = request.data.get("phoneNumber", "")
 
         # Send off twilio thing here
-        success = client.verify.v2.services(
+        twilio_response = client.verify.v2.services(
             service_sid
         ).verifications.create(to=phone_number, channel="sms")
-        return Response({"success": success.status == "pending"})
+        return Response({"status": twilio_response.status})
 
 
 class LoginOtpAPIView(APIView):
+    def addJWT(self, response):
+        # for testing, 12345 for id and 1 hour expiry, needs to be updated later.
+        # TODO: set these to better values (actual user id and end of the season)
+        payload = {
+            "user_id": "12345",
+            "exp": datetime.now(timezone.utc) + timedelta(hours=1),  
+        }
+        token = jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
+
+        response.set_cookie(
+                key="authToken",
+                value=token,
+                httponly=True,
+                secure=True,  
+                samesite="Lax",  
+                max_age=3600,  
+            )
+
     def post(self, request):
         # Probably a better way to get/validate things but I don't know or care what that is rn
         phone_number = request.data.get("phoneNumber", "")
         verification_code = request.data.get("verificationCode", "")
 
         # Send off twilio thing here
-        success = client.verify.v2.services(
-    service_sid
-).verification_checks.create(to=phone_number, code=verification_code)
-        return Response({"success": success.status == "approved"})
+        twilio_response = client.verify.v2.services(
+            service_sid
+        ).verification_checks.create(to=phone_number, code=verification_code)
+
+        response = Response({"status": twilio_response.status})
+
+        if(twilio_response.status == 'approved'):
+            self.addJWT(response)
+
+        return response
