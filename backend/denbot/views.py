813bab22d4f0d2ee0d3103cc4e8a89ef4911d847
Monique Streetman <moniquestreetman@gmail.com>
2025-12-11 20:39:49 -0700

    Fix docker compose ports, remove unnecessary file

--- backend/denbot/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-
-
-def index(request: HttpRequest) -> HttpResponse:
-    if request.user.is_anonymous:
-        return render(request, "denbot/public/index.html")
-    else:
-        return render(request, "denbot/authed/index.html")
