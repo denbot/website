d34af8f4557994ffbd2ababfc4e1d27f6794e346
Monique Streetman <85311336+mstreetm@users.noreply.github.com>
2026-01-28 13:15:28 -0700

    Strip /api prefix in nginx and backend (#15)

--- backend/denbot/urls.py
+++ backend/denbot/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
-import denbot.views
-
 urlpatterns = [
-    path("", denbot.views.index, name="index"),
-    path("api/auth/", include("authentication.urls")),
+    path("auth/", include("authentication.urls")),
 ]
