052099c080d134e6405d1fd4d724ffb417876e93
Monique Streetman <moniquestreetman@gmail.com>
2026-03-21 15:33:19 -0600

    Fix some style issues

--- backend/authentication/auth_suppliers/dev_auth_supplier.py
+++ backend/authentication/auth_suppliers/dev_auth_supplier.py
@@ -11,29 +11,26 @@ class DevAuthSupplier(AuthSupplier):
         expired_code: str = "000000",
         too_many_attempts_code: str = "429429",
     ) -> None:
-        self.phone_ending_attempts = phone_ending_attempts
-        self.approve_code = approve_code
-        self.failed_code = failed_code
-        self.expired_code = expired_code
-        self.too_many_attempts_code = too_many_attempts_code
+        self._phone_ending_attempts = phone_ending_attempts
+        self._approve_code = approve_code
+        self._failed_code = failed_code
+        self._expired_code = expired_code
+        self._too_many_attempts_code = too_many_attempts_code
 
     def send_code(self, phone_number: str) -> AuthStatus:
-        if (
-            phone_number[-len(self.phone_ending_attempts) :]
-            == self.phone_ending_attempts
-        ):
+        if phone_number.endswith(self._phone_ending_attempts):
             return AuthStatus.TOO_MANY_ATTEMPTS
         return AuthStatus.CREATED
 
     def verify_code(self, phone_number: str, verification_code: str) -> AuthStatus:
         match verification_code:
-            case self.approve_code:
+            case self._approve_code:
                 return AuthStatus.APPROVED
-            case self.failed_code:
+            case self._failed_code:
                 return AuthStatus.FAILED
-            case self.expired_code:
+            case self._expired_code:
                 return AuthStatus.EXPIRED
-            case self.too_many_attempts_code:
+            case self._too_many_attempts_code:
                 return AuthStatus.TOO_MANY_ATTEMPTS
             case _:
                 return AuthStatus.ERROR
--- backend/authentication/auth_suppliers/twilio_auth_supplier.py
+++ backend/authentication/auth_suppliers/twilio_auth_supplier.py
@@ -21,16 +21,16 @@ class TwilioAuth(AuthSupplier):
     def __init__(
         self, api_key: str, api_secret: str, account_sid: str, service_sid: str
     ) -> None:
-        self.api_key = api_key
-        self.api_secret = api_secret
-        self.account_sid = account_sid
-        self.service_sid = service_sid
-        self.client = Client(self.api_key, self.api_secret, self.account_sid)
+        self._api_key = api_key
+        self._api_secret = api_secret
+        self._account_sid = account_sid
+        self._service_sid = service_sid
+        self.client = Client(self._api_key, self._api_secret, self._account_sid)
 
     def send_code(self, phone_number: str) -> AuthStatus:
         try:
             twilio_response = self.client.verify.v2.services(
-                self.service_sid
+                self._service_sid
             ).verifications.create(to=phone_number, channel="sms")
             if twilio_response.status == TwilioResponse.PENDING:
                 return AuthStatus.CREATED
@@ -44,13 +44,14 @@ class TwilioAuth(AuthSupplier):
     def verify_code(self, phone_number: str, verification_code: str) -> AuthStatus:
         try:
             twilio_response = self.client.verify.v2.services(
-                self.service_sid
+                self._service_sid
             ).verification_checks.create(to=phone_number, code=verification_code)
 
             match twilio_response.status:
                 case TwilioResponse.APPROVED:
                     return AuthStatus.APPROVED
                 case TwilioResponse.PENDING:
+                    # The Twilio API returns 'pending' when a code is incorrect
                     return AuthStatus.FAILED
                 case TwilioResponse.MAX_ATTEMPTS_REACHED:
                     self.send_code(phone_number)
@@ -58,6 +59,8 @@ class TwilioAuth(AuthSupplier):
                 case TwilioResponse.EXPIRED:
                     self.send_code(phone_number)
                     return AuthStatus.EXPIRED
+                case _:
+                    return AuthStatus.ERROR
 
         except TwilioRestException as error:
             if error.status == 429:
