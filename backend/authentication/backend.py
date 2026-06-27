23901d7d66e9a33834e32cd065cccf1fd58ae0e0
Monique Streetman <moniquestreetman@gmail.com>
2026-06-27 17:27:21 -0600

    Start using auth backend (broken)

--- backend/authentication/backend.py
+++ backend/authentication/backend.py
@@ -2,25 +2,28 @@ from django.contrib.auth.backends import BaseBackend
 from django.http import HttpRequest
 
 from authentication.enums import AuthStatus
+from authentication.exceptions import OtpAuthError
 from authentication.models import DenbotUser
 from authentication.utils.auth import get_auth_supplier
 
 
 class OTPBackend(BaseBackend):
     def authenticate(
-        self, request: HttpRequest, username: str = None, password: str = None
+        self, request: HttpRequest, phone_number: str = None, otp: str = None
     ) -> DenbotUser:
-        status = get_auth_supplier().verify_code(username, password)
+        status = get_auth_supplier().verify_code(phone_number, otp)
 
         if status == AuthStatus.APPROVED:
             try:
-                return DenbotUser.objects.get(phone=username)
+                user = DenbotUser.objects.get(phone=phone_number)
+                return user
             except DenbotUser.DoesNotExist:
                 return None
-        return None
 
-    def get_user(self, user_id: str) -> DenbotUser:
+        raise OtpAuthError(status)
+
+    def get_user(self, phone_number: str) -> DenbotUser:
         try:
-            return DenbotUser.objects.get(pk=user_id)
+            return DenbotUser.objects.get(phone_number=phone_number)
         except DenbotUser.DoesNotExist:
             return None
--- backend/authentication/exceptions.py
+++ backend/authentication/exceptions.py
@@ -1,3 +1,8 @@
+from django.core.exceptions import PermissionDenied
+
+from authentication.enums import AuthStatus
+
+
 class JWTValidationError(Exception):
     pass
 
@@ -77,3 +82,8 @@ class InvalidConfigError(AuthImproperlyConfiguredError):
 
     def __str__(self) -> str:
         return "Invalid ${self.classname} config: ${self.error}"
+
+
+class OtpAuthError(PermissionDenied):
+    def __init__(self, status: AuthStatus) -> None:
+        self.status = status
