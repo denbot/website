02dea3c0ca4fecb1ff161044ef71f590ebdf5a9f
Monique Streetman <moniquestreetman@gmail.com>
2026-06-27 18:59:17 -0600

    update mui, fix connection in dev

--- backend/denbot/settings/local.py
+++ backend/denbot/settings/local.py
@@ -6,6 +6,8 @@ SECRET_KEY = "django-insecure-r5u$w6!mrp&95#k=tvr3djpq=8)hkp_u6!rt3)2l0_z7%twpw^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ["*"]
+
 AUTH_SUPPLIER = {
     "BACKEND": "authentication.auth_suppliers.dev_auth_supplier.DevAuthSupplier",
 }
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v16-appRouter';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import type { Metadata } from 'next';
